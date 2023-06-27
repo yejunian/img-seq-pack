d3dcda0ab7e0ad9d46ae137c6427c99b0b182f4b
yejunian <leeye51456@gmail.com>
2023-06-27 22:44:46 +0900

    Fix(main): Allow all of titles of the template

--- src/core/bundleData.ts
+++ src/core/bundleData.ts
@@ -2,7 +2,7 @@ async function bundleData(data: string, title?: string): Promise<Blob> {
   const response = await fetch('/template/template.html')
 
   let html = await response.text()
-  html = html.replace('[["title"]]', title || 'img-seq-pack')
+  html = html.replace(/\[\["title"\]\]/g, title || 'img-seq-pack')
   html = html.replace('[["data"]]', data)
 
   return new Blob([html], { type: 'text/html' })
