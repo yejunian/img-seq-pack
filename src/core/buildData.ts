ff14eb1984f3a8d32ee8df4e2290b2a6913753e7
yejunian <leeye51456@gmail.com>
2023-07-01 01:58:57 +0900

    Feat(main): Implement loose hashing

--- src/core/buildData.ts
+++ src/core/buildData.ts
@@ -1,26 +1,104 @@
+import sha1 from 'hash.js/lib/hash/sha/1'
 import type PackedData from './PackedData'
-import decodeImage, { DecodeResult } from './decodeImage'
+import decodeImage from './decodeImage'
+
+const imageDataKeys: (keyof ImageData)[] = ['width', 'height', 'data']
 
 async function buildData(fileList: FileList): Promise<PackedData> {
-  const hashToImageIndex = new Map<string, number>()
+  const hashToRefIndex = new Map<string, number>()
   const imageRefs: string[] = []
+  const refToFileIndex: number[] = []
   const pageList: number[] = []
 
-  for (const file of fileList ?? []) {
-    const { canvas, hash }: DecodeResult = await decodeImage(file)
+  for (let i = 0; i < fileList.length; i += 1) {
+    const canvas = await decodeImage(fileList[i])
+    const looseHash = hashImageData(getImageData(canvas, 64, 64))
 
-    if (hashToImageIndex.has(hash)) {
-      const dataIndex = hashToImageIndex.get(hash)!
-      pageList.push(dataIndex)
+    if (hashToRefIndex.has(looseHash)) {
+      const refIndex = hashToRefIndex.get(looseHash)!
+
+      if (refIndex >= 0) {
+        const fileIndex = refToFileIndex[refIndex]
+        const priorCanvas = await decodeImage(fileList[fileIndex])
+        const priorImageData = getImageData(priorCanvas)
+        const priorFullHash = hashImageData(priorImageData)
+
+        hashToRefIndex.set(priorFullHash, refIndex)
+        hashToRefIndex.set(looseHash, -1)
+      }
+
+      const currentImageData = getImageData(canvas)
+      const currentFullHash = hashImageData(currentImageData)
+
+      if (hashToRefIndex.has(currentFullHash)) {
+        const refIndex = hashToRefIndex.get(currentFullHash)!
+        pageList.push(refIndex)
+      } else {
+        const refIndex = createNewImageRef(i, canvas, currentFullHash)
+        pageList.push(refIndex)
+      }
     } else {
-      const dataIndex = imageRefs.length
-      hashToImageIndex.set(hash, dataIndex)
-      imageRefs.push(canvas.toDataURL('image/webp', 0.7))
-      pageList.push(dataIndex)
+      const refIndex = createNewImageRef(i, canvas, looseHash)
+      pageList.push(refIndex)
     }
   }
 
+  function createNewImageRef(
+    fileIndex: number,
+    canvas: HTMLCanvasElement,
+    hash: string
+  ): number {
+    const refIndex = imageRefs.length
+
+    hashToRefIndex.set(hash, refIndex)
+    imageRefs.push(canvas.toDataURL('image/webp', 0.7))
+    refToFileIndex.push(fileIndex)
+
+    return refIndex
+  }
+
   return { imageRefs, pageList }
+}
+
+function getImageData(
+  source: HTMLCanvasElement,
+  width?: number,
+  height?: number
+): ImageData {
+  let canvas = source
+  if (width && height && width > 0 && height > 0) {
+    canvas = document.createElement('canvas')
+    canvas.width = width
+    canvas.height = height
+  }
+
+  const ctx = canvas.getContext('2d')
+  if (!ctx) {
+    return new ImageData(canvas.width, canvas.height)
+  }
+
+  if (width && height) {
+    ctx.drawImage(source, 0, 0, canvas.width, canvas.height)
+  }
+
+  return ctx.getImageData(0, 0, canvas.width, canvas.height)
+}
+
+function hashImageData(imageData: ImageData): string {
+  const hashes: number[] = []
+
+  for (const key of imageDataKeys) {
+    const value = imageData[key]
+    const sha = sha1()
+    if (typeof value === 'number') {
+      sha.update(value.toString(16), 'hex')
+    } else {
+      sha.update(value)
+    }
+    hashes.push(...sha.digest())
+  }
+
+  return sha1().update(hashes).digest('hex')
 }
 
 export default buildData
--- src/core/decodeImage.ts
+++ src/core/decodeImage.ts
@@ -1,30 +1,17 @@
-import sha1 from 'hash.js/lib/hash/sha/1'
-
-export type DecodeResult = {
-  canvas: HTMLCanvasElement
-  hash: string
-}
-
 const decodableTypes = {
   ImageBitmap: new Set(['image/avif', 'image/webp', 'image/jpeg', 'image/png']),
 }
 
-const imageDataKeys: (keyof ImageData)[] = ['width', 'height', 'data']
-
-async function decodeImage(file: File): Promise<DecodeResult> {
-  let decoded: DecodeResult
-
-  if (decodableTypes.ImageBitmap.has(file.type)) {
-    decoded = await decodeByImageBitmap(file)
-  } else {
+async function decodeImage(file: File): Promise<HTMLCanvasElement> {
+  if (!decodableTypes.ImageBitmap.has(file.type)) {
     const extension = file.name.replace(/^.+\.(.+?)$/, '$1')
     throw new Error(`*.${extension} (${file.type}) is not supported.`)
   }
 
-  return decoded
+  return await decodeByImageBitmap(file)
 }
 
-async function decodeByImageBitmap(file: File): Promise<DecodeResult> {
+async function decodeByImageBitmap(file: File): Promise<HTMLCanvasElement> {
   const imageBitmap = await window.createImageBitmap(file)
   const { width, height } = imageBitmap
 
@@ -37,29 +24,7 @@ async function decodeByImageBitmap(file: File): Promise<DecodeResult> {
 
   imageBitmap.close()
 
-  const imageData =
-    ctx?.getImageData(0, 0, canvas.width, canvas.height) ??
-    new ImageData(canvas.width, canvas.height)
-  const hash = hashImageData(imageData)
-
-  return { canvas, hash }
-}
-
-function hashImageData(imageData: ImageData): string {
-  const hashes: number[] = []
-
-  for (const key of imageDataKeys) {
-    const value = imageData[key]
-    const messageDigest = sha1()
-    if (typeof value === 'number') {
-      messageDigest.update(value.toString(16), 'hex')
-    } else {
-      messageDigest.update(value)
-    }
-    hashes.push(...messageDigest.digest())
-  }
-
-  return sha1().update(hashes).digest('hex')
+  return canvas
 }
 
 export default decodeImage
