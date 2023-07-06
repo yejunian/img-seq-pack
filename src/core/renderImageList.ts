930a627d3aaede413f891a20f3255daef2e11391
yejunian <leeye51456@gmail.com>
2023-07-06 15:45:57 +0900

    Feat(template): Implement rendering page names

--- src/core/renderImageList.ts
+++ src/core/renderImageList.ts
@@ -1,13 +1,13 @@
 import type PackedData from './PackedData'
 import strictQuerySelector from './strictQuerySelector'
 
-function renderImageList({ images, pages }: PackedData): void {
+function renderImageList({ images, pages, names }: PackedData): void {
   const contentsRoot = strictQuerySelector(document, '#contents')
   const olElement = document.createElement('ol')
 
   for (let i = 0; i < pages.length; i += 1) {
     const divElement = document.createElement('div')
-    divElement.textContent = `${i + 1}.`
+    divElement.textContent = names[i]
 
     const imgElement = document.createElement('img')
     imgElement.src = images[pages[i]] ?? ''
