5d6c844469174cfcb54780d88479827ef4f058c4
yejunian <leeye51456@gmail.com>
2023-08-07 18:05:16 +0900

    Config: Add deploying scripts and options

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
+  base: '/img-seq-pack/',
+
   build: {
     rollupOptions: {
       external: ['canvg', 'html2canvas', 'dompurify'],
